c254381e3413d2f826b5016781d5a042473a40c2
Jai <jaikishannayak@gmail.com>
2026-02-22 16:01:51 +0530

    7: Accounts

--- src/app.js
+++ src/app.js
@@ -10,7 +10,9 @@ app.use(express.json())
  * - Routes required
  */
 const authRouter = require("./routes/auth.routes")
+const accountRouter = require("./routes/account.routes")
 
 app.use("/api/auth", authRouter)
+app.use("/api/accounts", accountRouter)
 
 module.exports = app
